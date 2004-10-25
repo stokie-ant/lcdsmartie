65d8adfb73844c5b6e64f2aa23d1f66338f26368
clansley <clansley>
2004-10-25 22:52:48 +0000

    Added BasieP's copyright and GPL license statement to what I think is BasieP's work.
    Added back the .res file - not sure this is auto generated from source.

--- lcdsmartie/Unit5.pas
+++ lcdsmartie/Unit5.pas
@@ -1,3 +1,25 @@
+{******************************************************************************
+ *
+ *  LCD Smartie - LCD control software.
+ *  Copyright (C) 2000-2003  BassieP
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *  $Source: /root/lcdsmartie-cvsbackup/lcdsmartie/Attic/Unit5.pas,v $
+ *  $Revision: 1.2 $ $Date: 2004/10/25 22:52:48 $
+ *****************************************************************************}
 unit Unit5;
 
 interface
