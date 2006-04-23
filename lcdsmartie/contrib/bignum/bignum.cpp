040ec13bcc08a953e70172fddb3d0194170b66d4
fast351 <fast351>
2006-04-23 12:14:26 +0000

    V2 with 2x2 font.  Different font sizes now are different function calls

--- lcdsmartie/contrib/bignum/bignum.cpp
+++ lcdsmartie/contrib/bignum/bignum.cpp
@@ -96,6 +96,124 @@ doDoubleHeight(unsigned int lineNum, unsigned int font, const char *text, string
 
 
 void
+doDoubleHeightWidth(unsigned int lineNum, unsigned int font, const char *text, string& outbuf)
+{
+	if (lineNum == 1)
+	{
+		// output font definition with the first line.
+		if (font == 0)
+			outbuf = ""; // let them make their own font
+		else if (font == 1) // standard
+			outbuf =  "$CustomChar(1,31,16,16,16,16,16,16,16)"  // reversed 7 shape
+					  "$CustomChar(2,31,1,1,1,1,1,1,1)"         // 7 shape
+					  "$CustomChar(3,16,16,16,16,16,16,16,31)"  // L shape
+					  "$CustomChar(4,1,1,1,1,1,1,1,31)"         // reversed L shape
+					  "$CustomChar(5,31,16,16,16,16,16,16,31)"  // C shape
+					  "$CustomChar(6,31,1,1,1,1,1,1,31)"        // reversed C shape
+					  "$CustomChar(7,0,0,0,0,0,0,0,31)"         // _
+					  "$CustomChar(8,31,0,0,0,0,0,0,0)";        // -
+
+    	else if (font == 2) // bold
+			outbuf =  "$CustomChar(1,31,31,24,24,24,24,24,24)"
+					  "$CustomChar(2,31,31,3,3,3,3,3,3)"
+					  "$CustomChar(3,24,24,24,24,24,24,31,31)"
+					  "$CustomChar(4,3,3,3,3,3,3,31,31)"
+					  "$CustomChar(5,31,31,24,24,24,24,31,31)"
+					  "$CustomChar(6,31,31,3,3,3,3,31,31)"
+					  "$CustomChar(7,0,0,0,0,0,0,31,31)"
+					  "$CustomChar(8,31,31,0,0,0,0,0,0)";
+		else if (font == 3) // standard, but only use last row if full row needed.
+			outbuf =  "$CustomChar(1,31,16,16,16,16,16,16,0)"  // reversed 7 shape
+					  "$CustomChar(2,31,1,1,1,1,1,1,0)"         // 7 shape
+					  "$CustomChar(3,16,16,16,16,16,16,16,31)"  // L shape
+					  "$CustomChar(4,1,1,1,1,1,1,1,31)"         // reversed L shape
+					  "$CustomChar(5,31,16,16,16,16,16,16,31)"  // C shape
+					  "$CustomChar(6,31,1,1,1,1,1,1,31)"        // reversed C shape
+					  "$CustomChar(7,0,0,0,0,0,0,0,31)"         // _
+					  "$CustomChar(8,31,0,0,0,0,0,0,0)";        // -
+		else if (font == 4) // bold, but only use last row if full row needed.
+			outbuf =  "$CustomChar(1,31,31,24,24,24,24,24,0)"
+					  "$CustomChar(2,31,31,3,3,3,3,3,0)"
+					  "$CustomChar(3,24,24,24,24,24,24,31,31)"
+					  "$CustomChar(4,3,3,3,3,3,3,31,31)"
+					  "$CustomChar(5,31,31,24,24,24,24,31,31)"
+					  "$CustomChar(6,31,31,3,3,3,3,31,31)"
+					  "$CustomChar(7,0,0,0,0,0,0,31,31)"
+					  "$CustomChar(8,31,31,0,0,0,0,0,0)";
+		else 
+		{
+			outbuf = "[bignum: no such font]";
+			return;
+		}
+	}
+	else
+		outbuf = "";
+
+	const unsigned char CC1=176, CC2=158, CC3=131, CC4=132, CC5=133, CC6=134, CC7=135, CC8=136, SP=' ';
+
+	unsigned char fontmap[10][2][2] = {
+		/*0*/{
+				{CC1,  CC2},
+				{CC3,  CC4}
+			},
+		/*1*/{
+				{CC2,   SP},
+				{CC4,  CC7}
+			},
+		/*2*/{
+				{CC8,  CC6},
+				{CC5,  CC7}
+			},
+		/*3*/{
+				{CC8,  CC6},
+				{CC7,  CC4}
+			},
+		/*4*/{
+				{CC3,  CC4},
+				{ SP,  CC2}
+			},
+		/*5*/{
+				{CC5,  CC8},
+				{CC7,  CC6}
+			},
+		/*6*/{
+				{CC1,  CC8},
+				{CC5,  CC6}
+			},
+		/*7*/{
+				{CC8,  CC2},
+				{ SP,  CC2}
+			},
+		/*8*/{
+				{CC5,  CC6},
+				{CC3,  CC4}
+			},
+		/*9*/{
+				{CC5,  CC6},
+				{CC7,  CC4}
+			}
+	};
+
+	for (unsigned int i=0; i < strlen(text); i++)
+	{
+		if ((text[i] >= '0') && (text[i] <='9'))
+		{
+			outbuf += fontmap[text[i]-'0'][lineNum-1][0];
+			outbuf += fontmap[text[i]-'0'][lineNum-1][1];
+		}
+		else if (text[i] == ':')
+			outbuf += '.';
+		else if ((text[i] == '-') && (lineNum == 1)) 
+			outbuf += CC7;
+		else if ((lineNum == 2) && (text[i] != ':') && (text[i] != '-'))
+			outbuf += text[i];
+		else 
+			outbuf += ' ';
+	}
+}
+
+
+void
 doQuadHeight(unsigned int lineNum,  unsigned int font, const char *text, string& outbuf)
 {
 	if (lineNum == 1)
@@ -130,7 +248,7 @@ doQuadHeight(unsigned int lineNum,  unsigned int font, const char *text, string&
 	else
 		outbuf = "";
 
-	const TH=176, BH=158, FB=131, RH=132, TLC=133, BRC=134, TRC=135, BLC=136, SP=' ';
+	const unsigned char TH=176, BH=158, FB=131, RH=132, TLC=133, BRC=134, TRC=135, BLC=136, SP=' ';
 	
 
 	unsigned char fontmap[10][4][3] = {
@@ -216,11 +334,11 @@ doQuadHeight(unsigned int lineNum,  unsigned int font, const char *text, string&
 
 /*********************************************************
  *         Function 1                                    *
- * Draws big numbers.                                    *
- *  param1 - [linenum#maxlines[#font]]                   *
+ * Draws big numbers. (1x2 font)                         *
+ *  param1 - [linenum[#font]]                            *
  *  param2 - text to make big.                           *
- *	eg: $dll(bignum,1#2,12:35)                           *
- *	    $dll(bignum,2#2,12:35)                           *
+ *	eg: $dll(bignum,1,12:35)                             *
+ *	    $dll(bignum,2,12:35)                             *
  *********************************************************/
 extern "C" BIGNUM_API  char * 
 __stdcall  function1(const char *param1, const char *param2)
@@ -231,19 +349,6 @@ __stdcall  function1(const char *param1, const char *param2)
 
 	const char *hash = strchr(param1, '#');
 	
-	// handle maxLines
-	unsigned int maxLines;
-	if (hash)
-		hash = strchr(hash, '#');
-	
-	if (hash != NULL)
-	{
-		hash ++;
-		maxLines = atoi(hash);
-	}
-	else
-		maxLines = 2;
-
     // handle font
 	unsigned int font;
 	if (hash)
@@ -257,18 +362,89 @@ __stdcall  function1(const char *param1, const char *param2)
 	else
 		font = 1;
 
-	if ((lineNum < 1) || (lineNum > maxLines))
+	if ((lineNum < 1) || (lineNum > 2))
 		return "[bignum: invalid line number]";
 
-	if (maxLines == 2)
-		doDoubleHeight(lineNum, font, param2, outbuf);
-	else if (maxLines == 4)
-		doQuadHeight(lineNum, font, param2, outbuf);
-	else
-	{
-		return "[bignum: unsupported number of lines]";
-	}
-
+    doDoubleHeight(lineNum, font, param2, outbuf);
  
 	return const_cast<char *>(outbuf.c_str());
 }
+
+/*********************************************************
+ *         Function 2                                    *
+ * Draws big numbers. (2x2 font)                         *
+ *  param1 - [linenum[#font]]                            *
+ *  param2 - text to make big.                           *
+ *	eg: $dll(bignum,1,12:35)                             *
+ *	    $dll(bignum,2,12:35)                             *
+ *********************************************************/
+extern "C" BIGNUM_API  char * 
+__stdcall  function2(const char *param1, const char *param2)
+{ 
+	static string outbuf;
+
+	unsigned int lineNum = atoi(param1);
+
+	const char *hash = strchr(param1, '#');
+	
+    // handle font
+	unsigned int font;
+	if (hash)
+		hash = strchr(hash, '#');
+	
+	if (hash != NULL)
+	{
+		hash ++;
+		font = atoi(hash);
+	}
+	else
+		font = 1;
+
+	if ((lineNum < 1) || (lineNum > 2))
+		return "[bignum: invalid line number]";
+
+    doDoubleHeightWidth(lineNum, font, param2, outbuf);
+ 
+	return const_cast<char *>(outbuf.c_str());
+}
+
+/*********************************************************
+ *         Function 3                                    *
+ * Draws big numbers.   (3x4 font)                       *
+ *  param1 - [linenum[#font]]                            *
+ *  param2 - text to make big.                           *
+ *	eg: $dll(bignum,1,12:35)                             *
+ *	    $dll(bignum,2,12:35)                             *
+ *	    $dll(bignum,3,12:35)                             *
+ *	    $dll(bignum,4,12:35)                             *
+ *********************************************************/
+extern "C" BIGNUM_API  char * 
+__stdcall  function3(const char *param1, const char *param2)
+{ 
+	static string outbuf;
+
+	unsigned int lineNum = atoi(param1);
+
+	const char *hash = strchr(param1, '#');
+	
+    // handle font
+	unsigned int font;
+	if (hash)
+		hash = strchr(hash, '#');
+	
+	if (hash != NULL)
+	{
+		hash ++;
+		font = atoi(hash);
+	}
+	else
+		font = 1;
+
+	if ((lineNum < 1) || (lineNum > 4))
+		return "[bignum: invalid line number]";
+
+    doQuadHeight(lineNum, font, param2, outbuf);
+ 
+	return const_cast<char *>(outbuf.c_str());
+}
+
